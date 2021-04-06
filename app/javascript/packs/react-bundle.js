cdbf9dcdce0c570fcd178393333290ffc79d90d9
Khalil Najjar <khn@alteos.com>
2021-04-06 12:36:18 -0600

    remove menu

--- app/javascript/packs/react-bundle.js
+++ app/javascript/packs/react-bundle.js
@@ -1,9 +1,7 @@
 import ReactOnRails from 'react-on-rails';
 
-import Menu from '../bundles/components/Menu';
 import GameForm from '../bundles/components/GameForm';
 
 ReactOnRails.register({
-  Menu,
   GameForm,
 });
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :host, class_name: 'User', foreign_key: 'host_id'
-  belongs_to :guest, class_name: 'User', foreign_key: 'guest_id'
+  belongs_to :guest, class_name: 'User', foreign_key: 'guest_id', optional: true
 
   validates :name, presence: true
   validates :password, presence: true
