e106dd911196260f7053bda03202eee861757e62
Khalil Najjar <khn@alteos.com>
2021-04-07 08:19:21 -0600

    add can_make_move method

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,8 +6,16 @@ class Game < ApplicationRecord
   enum status: %i[settled in_progress]
 
   def player_move(player:, position:)
+    return unless can_make_move?(position)
+
     guest_moves << position if player == 'guest'
     host_moves << position if player == 'host'
     save!
+  end
+
+  private
+
+  def can_make_move?(position)
+    guest_moves.exclude?(position) && host_moves.exclude?(position)
   end
 end
